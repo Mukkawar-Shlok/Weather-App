731988048102bdd1d33eca2863bab06d1daac765
Shlok M Mukkawar <shlokmukkawar2001@gmail.com>
2022-04-02 21:50:12 +0530

    Weather api project

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const request = require("request");
+
+const app = express();
+app.set('view engine', 'ejs')
+var city = 'Mumbai'
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=94c58afacec910ab10262578a14f4815`
+
+app.get('/', (req, res) => {
+
+    request(url, function (error, response, body) {
+        weather = JSON.parse(body);
+        // console.log(weather);
+        let weather_val = {
+            city: city,
+            temperature: weather.main.temp,
+            min_temp: weather.main.temp_min,
+            max_temp: weather.main.temp_max,
+            des: weather.weather[0].description,
+            icon: weather.weather[0].icon
+        }
+
+        var weather_data = { weather: weather_val }
+        // console.log(weather_data);
+
+        res.render('home', weather_data);
+    })
+
+})
+
+app.get('/post', (req, res) => {
+    const { search } = req.query
+    city = search
+    console.log(city);
+    request(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=94c58afacec910ab10262578a14f4815`, function (error, response, body) {
+        weather = JSON.parse(body);
+        // console.log(weather);
+        let weather_val = {
+            city: city,
+            temperature: weather.main.temp,
+            min_temp: weather.main.temp_min,
+            max_temp: weather.main.temp_max,
+            des: weather.weather[0].description,
+            icon: weather.weather[0].icon
+        }
+
+        var weather_data = { weather: weather_val }
+        // console.log(weather_data);
+
+        res.render('home', weather_data);
+    })
+
+})
+
+app.listen(3000, () => console.log("listening on port 3000"));
